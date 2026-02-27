8777d0aa4364ebc7ff117701cf172436643be575
Vassja <v.misenko@web.de>
2026-02-27 13:46:24 +0100

    cleaned css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <h1>Hello World!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="app-body">
+      <div className="bento">
+        TODO: add components here
       </div>
-    </>
+    </div>
   )
 }
 
