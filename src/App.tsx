313dd6dd2457a198e0e19ea8b898361a1d270968
Vassja <v.misenko@web.de>
2026-03-01 15:23:44 +0100

    changed app.tsx with cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import ProfileCard from "./components/ProfileCard";
+import StreakCard from './components/StreakCard'
+import QuoteCard from './components/QuoteCard'
+import ScoreCard from './components/ScoreCard'
+import HistoryCard from './components/HistoryCard'
+import SkipCard from './components/SkipCard'
+import AboutCard from './components/AboutCard'
 
 function App() {
   return (
     <div className="app-body">
       <div className="bento">
         <ProfileCard lastCorrect={state.lastCorrect} phase={state.phase} />
+
+        <StreakCard streak={state.streak} />
+
+        <QuoteCard
+          currentQuote={currentQuote}
+          loading={loading}
+          phase={state.phase}
+          quoteIndex={state.quoteIndex}
+          lastCorrect={state.lastCorrect}
+          onAnswer={answer}
+        />
+
+        <ScoreCard score={state.score} played={state.played} />
+
+        <HistoryCard history={state.history} />
+
+        <SkipCard onSkip={handleSkip} />
+
+        <AboutCard />
       </div>
     </div>
   );
