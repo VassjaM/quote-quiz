4de64caa602ef14860780b6873862272e3bfac1d
Vassja <v.misenko@web.de>
2026-03-06 16:01:44 +0100

    changed titel and subtitel

--- src/components/GameTitle.tsx
+++ src/components/GameTitle.tsx
@@ -1,8 +1,8 @@
 export default function GameTitle() {
   return (
     <div className="game-title">
-      <h1 className="game-title-main">Real or Fake?</h1>
-      <p className="game-title-sub">Can you spot a real Kanye quote?</p>
+      <h1 className="game-title-main">Kanye Said What?</h1>
+      <p className="game-title-sub">Can you tell the difference?</p>
     </div>
   )
 }
